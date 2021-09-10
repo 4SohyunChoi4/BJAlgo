2e7eb79d83a252a77cfa458a1f5388dde47fadda
unknown <sagie.choi@gmail.com>
2021-09-11 01:47:27 +0900

    10869

---
+++ 10869.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    int c;
+    scanf("%d %d",&a, &b);
+    printf("%d\n",a+b);
+    printf("%d\n",a-b);
+    printf("%d\n",a*b);
+    printf("%d\n",a/b);
+    printf("%d\n",a%b);
+    return 0;
+}
