13d038128759829c2f94a01e8b303e203913ae42
unknown <4shchoi4@gmail.com>
2021-09-12 22:58:27 +0900

    10430

---
+++ 10430.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+//첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다
+    int A, B, C;
+    cin>>A>>B>>C;
+    cout<<(A+B)%C<<endl;
+    cout<<((A%C) + (B%C))%C<<endl;
+    
+    cout<< (A*B)%C<<endl;
+    cout<<((A%C) * (B%C))%C<<endl;
+    return 0;
+}
