1f34af2264af49dfe968c171cc655a39d70220aa
unknown <4shchoi4@gmail.com>
2021-09-13 23:28:05 +0900

    2588

---
+++ 2588.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+int main(){
+    int a;
+    string b;
+    cin>>a>>b;
+    cout<<a*stoi(b.substr(2))<<endl<<a*stoi(b.substr(1,1))<<endl<<a*stoi(b.substr(0,1))<<endl;
+    cout << a*stoi(b);
+    
+    return 0;
+}
