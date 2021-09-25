99e8e46aa67583aeb0f0f6940bd10f7ff9d7e265
unknown <sagie.choi@gmail.com>
2021-09-25 23:19:03 +0900

    2742

---
+++ 2742.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//using namespace std;
+int main()
+{
+    int N;
+    //cin>>N;
+    scanf("%d",&N);
+    for(int i=N ; i>0 ; i--)
+        printf("%d\n",i);
+        //cout<<i<<endl;
+    return 0;
+}
