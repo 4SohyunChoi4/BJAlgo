5cc8115c5e9b77eeb216f78fe153bcdd92a418e8
unknown <sagie.choi@gmail.com>
2022-02-10 17:26:43 +0900

    암기왕_binary search로 수정

--- 2776.cpp
+++ 2776.cpp
@@ -1,65 +1,56 @@
 #include <stdio.h>
-#include <set>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
-/*
-int binarySearch(int input, set<int> note, int low, int high){
+
+vector<int> note1;
+
+int binarySearch(int input, int low, int high){
     if(low > high)
         return 0;
     else
     {
         int mid = ( low+high )/2;
-        //cout<<low<<" "<<mid<<" "<<high<<endl;
-        if ( note[mid] == input)
+        if ( note1[mid] == input)
         {
             return 1;
         }
-        else if ( note[mid] > input)
+        else if ( note1[mid] > input)
         {
-           return binarySearch(input, note, low, mid-1);
+           return binarySearch(input, low, mid-1);
         }
         else
         {
-           return binarySearch(input, note, mid+1, high);
+           return binarySearch(input, mid+1, high);
         }
     }
 }
-*/
+
 int main()
 {
-    //ios::sync_with_stdio(false);
-    //cin.tie(NULL);
-    //cout.tie(NULL);
-        
     int T;
-    //cin>>T;
     scanf("%d",&T);
     
-    int k=0;
-    while(k<T)
+    while(T--)
     {
         int M, N, temp;
-        set<int> note1;
         
-        //cin>>N;
-        scanf("%d",&N);
+       scanf("%d",&N);
         for(int i=0 ; i<N ; i++)
         {   
-            //cin>>temp;
             scanf("%d",&temp);
-            note1.insert(temp);
+            note1.emplace_back(temp);
         }
-        //cin>>M;
+        sort(note1.begin(), note1.end());
         scanf("%d",&M);
+        int temp2;
         for(int i=0 ; i<M ; i++)
         {   
-            //cin>>temp;
-            scanf("%d",&temp);
-            note1.count(temp)>0 ? puts("1") : puts("0");
-            //cout<<binarySearch(temp, note1, 0, M-1)<<endl;
-        }   
-        k++;
+            scanf("%d",&temp2);
+            printf("%d\n", binarySearch(temp2, 0, N-1));
+        }
+        note1.clear();
     }
 
     return 0;
